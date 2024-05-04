8c8c3ddb8c603025ec9f07d74c98bb2789eb2e27
Jiahui Zhang <jiahzhang@student.ethz.ch>
2024-05-04 11:21:26 +0000

    add dependency

---
+++ prepare.sh
@@ -0,0 +1,4 @@
+cd ~
+wget https://download.swift.org/swift-5.9.2-release/ubuntu2204/swift-5.9.2-RELEASE/swift-5.9.2-RELEASE-ubuntu22.04.tar.gz
+tar zxvf ./swift-5.9.2-RELEASE-ubuntu22.04.tar.gz
+echo 'export PATH=~/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}' >> ~/.bashrc
