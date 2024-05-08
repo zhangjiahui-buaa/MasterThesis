5dba82bfe3ce5d9d2845cfa793151e3aa5a0e492
zhangjiahui-buaa <17231043@buaa.edu.cn>
2024-05-08 16:27:58 +0200

    fix

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,22 @@
 cd /home/jiahui
+sudo apt-get install \
+          binutils \
+          git \
+          gnupg2 \
+          libc6-dev \
+          libcurl4-openssl-dev \
+          libedit2 \
+          libgcc-9-dev \
+          libpython3.8 \
+          libsqlite3-0 \
+          libstdc++-9-dev \
+          libxml2-dev \
+          libz3-dev \
+          pkg-config \
+          tzdata \
+          unzip \
+          zlib1g-dev
 wget https://download.swift.org/swift-5.9.2-release/ubuntu2204/swift-5.9.2-RELEASE/swift-5.9.2-RELEASE-ubuntu22.04.tar.gz
 tar zxvf ./swift-5.9.2-RELEASE-ubuntu22.04.tar.gz
 export PATH=/home/jiahui/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}
-echo 'export PATH=/home/jiahui/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}' >> ~/.bashrc
+echo 'export PATH=/home/jiahui/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}' >> /home/jiahui/.bashrc
