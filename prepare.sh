4248a2e0438bc0dc2596dac6a0dfdb0b6b61583d
zhangjiahui-buaa <17231043@buaa.edu.cn>
2024-05-08 16:07:15 +0200

    fix

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
-cd ~
+cd /home/jiahui
 wget https://download.swift.org/swift-5.9.2-release/ubuntu2204/swift-5.9.2-RELEASE/swift-5.9.2-RELEASE-ubuntu22.04.tar.gz
 tar zxvf ./swift-5.9.2-RELEASE-ubuntu22.04.tar.gz
-export PATH=~/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}
-echo 'export PATH=~/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}' >> ~/.bashrc
+export PATH=/home/jiahui/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}
+echo 'export PATH=/home/jiahui/swift-5.9.2-RELEASE-ubuntu22.04/usr/bin:${PATH}' >> ~/.bashrc
