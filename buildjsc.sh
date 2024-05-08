8d5327a7d3a77d2362eb44fb5ac251fb19d799d8
zhangjiahui-buaa <17231043@buaa.edu.cn>
2024-05-08 15:27:39 +0200

    fix

--- buildjsc.sh
+++ buildjsc.sh
@@ -1,11 +1,11 @@
 sudo apt update 
 sudo apt install docker.io
-cd ~
+cd /home/jiahui
 git clone https://github.com/googleprojectzero/fuzzilli.git
-bash ~/MasterThesis/prepare.sh
-cd ~/MasterThesis
+bash /home/jiahui/MasterThesis/prepare.sh
+cd /home/jiahui/MasterThesis
 bash Cloud/Docker/build.sh jsc
 tmux new "taskset -c 0,1,2,3 swift run -c release FuzzilliCli --profile=jsc --timeout=1000 --storagePath=./jsc --jobs=4 --minimizationLimit=0.2 ~/MasterThesis/Cloud/Docker/JSCBuilder/out/jsc > JSCJIT.log"
 
-cd ~/fuzzilli
+cd /home/jiahui/fuzzilli
 tmux new "taskset -c 4,5,6,7 swift run -c release FuzzilliCli --profile=jsc --timeout=1000 --storagePath=./jsc --jobs=4  ~/MasterThesis/Cloud/Docker/JSCBuilder/out/jsc > JSC.log"
